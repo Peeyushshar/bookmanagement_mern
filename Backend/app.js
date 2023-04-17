8703baee88dd7f7a853a846f7fe448e68383ee83
Peeyushshar <peeyushsharma996633pcs@gmail.com>
2023-04-17 17:03:51 +0530

    book management

---
+++ Backend/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const router = require("./routes/book-routes");
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use("/books", router)
+
+mongoose.connect("mongodb+srv://peeyushsharma996633pcs:Peeyush123@cluster0.uonehd8.mongodb.net/bookStore?retryWrites=true&w=majority").then(()=>console.log("Connected To Database"))
+.then(()=>{
+    app.listen(5000);
+}).catch((err)=> console.log(err));
